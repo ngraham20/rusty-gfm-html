826de60bcda5b1f54078d2828a543f6021aa513d
Nathaniel Graham <ngraham20@my.whitworth.edu>
2020-09-05 22:41:47 -0600

    app works, no helpful errors though

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,56 @@
 use comrak::{markdown_to_html, ComrakOptions};
-use clap::{Arg, App, SubCommand};
+use clap::{Arg, App};
 
 fn main() {
-    parse_args();
-}
-
-enum Options {
-    OutFile(String),
-    StyleFile(String)
+    match parse_args() {    
+        Ok((markdown, styles, outfile)) => { 
+            match convert(markdown, styles, outfile) {
+                Ok(_) => {},
+                Err(message) => println!("{}", message)
+            }
+        },
+        Err(message) => println!("{}", message)
+    }
 }
 
 fn parse_args() -> Result<(String, String, String), std::io::Error> {
 
-    let args = std::env::args();
-
-    if args.len() < 2 {
-        usage();
-    }
-
-    let markdown = std::fs::read_to_string(infile).expect(format!("Could not read {}", infile));
-    let styles: String;
-    if let Ok(res) = std::fs::read_to_string(styles) {
-        styles = res;
-    }
-    else {
-        styles = std::fs::read_to_string("styles.css").expect("Could not find styles.css");
-    };
+    let matches = App::new("RUSTY-GFM-HTML")
+        .version("1.0")
+        .about("The Rusty Github Flavored Markdown HTML Generator!")
+        .author("Nathaniel Graham <nathaniel.graham@protonmail.com>")
+        .arg(Arg::with_name("infile")
+            .value_name("INFILE")
+            .required(true)
+            .takes_value(true))
+        .arg(Arg::with_name("outfile")
+            .short("o")
+            .long("outfile")
+            .value_name("OUTFILE")
+            .help("output file to save the generated html to")
+            .default_value("README.html")
+            .takes_value(true))
+        .arg(Arg::with_name("styles")
+            .short("s")
+            .long("styles")
+            .value_name("STYLES")
+            .help("The styles file to use")
+            .default_value("styles.css")
+            .takes_value(true))
+        .get_matches();
+    
+    let mdfile = matches.value_of("infile").unwrap();
+    let markdown = std::fs::read_to_string(&mdfile)?;
+    let stfile = matches.value_of("styles").unwrap();
+    let styles = std::fs::read_to_string(stfile)?;
+    let outfile = String::from(matches.value_of("outfile").unwrap());
+    
+    // TODO fix error handeling
+    // match (markdown, styles, outfile) {
+    //     (Ok(md), Ok(st), ot) => Ok((md, st, ot)),
+    //     (Err(md), _, _) => Err(),
+    //     (_, Err(st), _) => Err(st)
+    // }
 
     Ok((markdown, styles, outfile))
 }
@@ -54,18 +79,6 @@ fn convert(markdown: String, styles: String, outfile: String) -> Result<(), std:
     let content = markdown_to_html(&markdown, &options);
     let html = header + &content + &footer;
 
-    std::fs::write(outfile, &html).expect(format!("Failed to write to {}", outfile));
+    std::fs::write(&outfile, &html).expect(&format!("Failed to write to {}", outfile));
     Ok(())
-}
-
-fn usage() {
-    let options = "
-    -o | --outfile  : The output file to save the generated html to (default is README.html)\n
-    -s | --styles   : The styles file to use (default is styles.css)
-    ";
-    let program_header = "RUSTY-GFM-HTML: The Rusty Github Flavored Markdown HTML Generator!";
-    let copyright = "Copyright (c) 2020 Nathaniel Graham";
-    let instructions = "Usage: rusty-gfm-html FILE [OPTIONS]";
-
-    println!("{}\n{}\n\n{}", program_header, instructions, options);
 }
